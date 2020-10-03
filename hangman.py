11bd06396e66e9d7195f58473fb952e63f26281b
Luka Klein <56469760+lklein2505@users.noreply.github.com>
2020-10-03 12:00:00 +0200

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,40 @@
+def draw(c):
+    if c == 7:
+        print("-------")
+        print("-------")
+        print("-------")
+    elif c == 6:
+        print("---O---")
+        print("-------")
+        print("-------")
+    elif c == 5:
+        print("---O---")
+        print("---|---")
+        print("-------")
+    elif c == 4:
+        print("---O---")
+        print("--/|---")
+        print("-------")
+    elif c == 3:
+        print("---O---")
+        print("--/|---")
+        print("-------")
+    elif c == 2:
+        print("---O---")
+        print("--/|\--")
+        print("-------")
+    elif c == 1:
+        print("---O---")
+        print("--/|\--")
+        print("--/----")
+    elif c == 0:
+        print("---O---")
+        print("--/|\--")
+        print("--/-\--")
+        print("Bravo luzeru, izgubio si")
+        
+        
+        
+              
+              
+
